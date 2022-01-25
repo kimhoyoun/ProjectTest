1176da1d899953de5c077930272634e414fb3903
kimhoyoun <hoyoun1309@gmail.com>
2022-01-25 17:35:13 +0900

    dao => db insert, update, delete 구현

--- src/org/model/GameDataDto.java
+++ src/org/model/GameDataDto.java
@@ -1,24 +1,128 @@
 package org.model;
 
 public class GameDataDto {
-	double game1;
-	double game2;
-	double game3;
-	double game4;
-	double game5;
+	String id;
+	int ansGame1;
+	int totalGame1;
+	int ansGame2;
+	int totalGame2;
+	int ansGame3;
+	int totalGame3;
+	int ansGame4;
+	int totalGame4;
+	int ansGame5;
+	int totalGame5;
+	
 	String day;
 	
 	public GameDataDto() {
-		this(0,0,0,0,0,"");
+		this("", 0,0,0,0,0,0,0,0,0,0,"");
 	}
 	
-	public GameDataDto(double game1, double game2, double game3, double game4, double game5, String day) {
-		this.game1 = game1;
-		this.game2 = game2;
-		this.game3 = game3;
-		this.game4 = game4;
-		this.game5 = game5;
+	public GameDataDto(String id, int ansGame1, int totalGame1,int ansGame2, int totalGame2, int ansGame3, int totalGame3, int ansGame4, int totalGame4, int ansGame5, int totalGame5, String day) {
+		this.id = id;
+		this.ansGame1 = ansGame1;
+		this.totalGame1 = totalGame1;
+		this.ansGame2 = ansGame2;
+		this.totalGame2 = totalGame2;
+		this.ansGame3 = ansGame3;
+		this.totalGame3 = totalGame3;
+		this.ansGame4 = ansGame4;
+		this.totalGame4 = totalGame4;
+		this.ansGame5 = ansGame5;
+		this.totalGame5 = totalGame5;
+		
 		this.day = day;
+	}
+
+	
+	
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public int getAnsGame1() {
+		return ansGame1;
+	}
+
+	public void setAnsGame1(int ansGame1) {
+		this.ansGame1 = ansGame1;
+	}
+
+	public int getTotalGame1() {
+		return totalGame1;
+	}
+
+	public void setTotalGame1(int totalGame1) {
+		this.totalGame1 = totalGame1;
+	}
+
+	public int getAnsGame2() {
+		return ansGame2;
+	}
+
+	public void setAnsGame2(int ansGame2) {
+		this.ansGame2 = ansGame2;
+	}
+
+	public int getTotalGame2() {
+		return totalGame2;
+	}
+
+	public void setTotalGame2(int totalGame2) {
+		this.totalGame2 = totalGame2;
+	}
+
+	public int getAnsGame3() {
+		return ansGame3;
+	}
+
+	public void setAnsGame3(int ansGame3) {
+		this.ansGame3 = ansGame3;
+	}
+
+	public int getTotalGame3() {
+		return totalGame3;
+	}
+
+	public void setTotalGame3(int totalGame3) {
+		this.totalGame3 = totalGame3;
+	}
+
+	public int getAnsGame4() {
+		return ansGame4;
+	}
+
+	public void setAnsGame4(int ansGame4) {
+		this.ansGame4 = ansGame4;
+	}
+
+	public int getTotalGame4() {
+		return totalGame4;
+	}
+
+	public void setTotalGame4(int totalGame4) {
+		this.totalGame4 = totalGame4;
+	}
+
+	public int getAnsGame5() {
+		return ansGame5;
+	}
+
+	public void setAnsGame5(int ansGame5) {
+		this.ansGame5 = ansGame5;
+	}
+
+	public int getTotalGame5() {
+		return totalGame5;
+	}
+
+	public void setTotalGame5(int totalGame5) {
+		this.totalGame5 = totalGame5;
 	}
 
 	public String getDay() {
@@ -28,76 +132,14 @@ public class GameDataDto {
 	public void setDay(String day) {
 		this.day = day;
 	}
-
-	public double getGame1() {
-		return game1;
-	}
-
-	public void setGame1(double game1) {
-		this.game1 = game1;
-	}
-
-	public double getGame2() {
-		return game2;
-	}
-
-	public void setGame2(double game2) {
-		this.game2 = game2;
-	}
-
-	public double getGame3() {
-		return game3;
-	}
-
-	public void setGame3(double game3) {
-		this.game3 = game3;
-	}
-
-	public double getGame4() {
-		return game4;
-	}
-
-	public void setGame4(double game4) {
-		this.game4 = game4;
-	}
-
-	public double getGame5() {
-		return game5;
-	}
-
-	public void setGame5(double game5) {
-		this.game5 = game5;
-	}
-
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((day == null) ? 0 : day.hashCode());
-		return result;
-	}
-
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		GameDataDto other = (GameDataDto) obj;
-		if (day == null) {
-			if (other.day != null)
-				return false;
-		} else if (!day.equals(other.day))
-			return false;
-		return true;
-	}
 	
 	@Override
 	public String toString() {
-		return String.format("%s : %3.2f%%  %3.2f%%  %3.2f%%  %3.2f%%  %3.2f%%",day,game1,game2,game3,game4,game5);
+		return String.format("%s : %3.2f%%  %3.2f%%  %3.2f%%  %3.2f%%  %3.2f%%",day,(totalGame1 == 0)?0: ((double) ansGame1/totalGame1)*100,(totalGame2 == 0)?0: ((double)ansGame2/totalGame2)*100,(totalGame3 == 0)?0: ((double)ansGame3/totalGame3)*100,(totalGame4 == 0)?0: ((double)ansGame4/totalGame4)*100,(totalGame5 == 0)?0: ((double)ansGame5/totalGame5)*100);
 	}
 	
 	
 }
+
+
+
--- src/org/test/DaoTest.java
+++ src/org/test/DaoTest.java
@@ -9,8 +9,72 @@ import org.model.UserDao;
 import org.model.UserDto;
 
 public class DaoTest {
-	public static void main(String[] args) {
-		UserDao dao = new UserDao();
+	public static UserDao dao = new UserDao();
+	
+	public static void main_deleteTest(String[] args) {
+		boolean ok = dao.deleteUser(new UserDto(0, null, "user4", null, 0));
+		
+		if(ok) {
+			System.out.println("user 정보 삭제!");
+		}
+		
+		ok = dao.deleteGameData(new GameDataDto("user4",0,0,0,0,0,0,0,0,0,0,null));
+		
+		if(ok) {
+			System.out.println("gameData 정보 삭제!");
+		}
+	}
+	
+	public static void main_updateTest(String[] args) {
+		boolean ok = dao.updateUser(new UserDto(0, "saram4_1", "user4", "user4", 78));
+		if(ok) {
+			System.out.println("수정 성공");
+		}
+			
+//		boolean ok = dao.updateGameData(new GameDataDto("user4",17,20,26,30,17,20,22,30,31,40,"2022-01-13"));
+//		if(ok) {
+//			System.out.println("수정 성공");
+//		}
+	}
+	
+	public static void main_insertTest(String[] args) {
+//		boolean ok = dao.insertUser(new UserDto(0, "saram4", "user4", "user4", 77));
+//		if(ok) {
+//			System.out.println("입력 성공");
+//		}
+		
+		System.out.println("================== roadUser Test ==================");
+		dao.roadUser();
+		for(UserDto dto : dao.userVector) {
+			System.out.println(dto);
+		}
+		
+//		boolean ok = dao.insertGameData(new GameDataDto("user4",8,10,16,20,9,10,15,20,25,30,"2022-01-13"));
+//		if(ok) {
+//			System.out.println("입력성공");
+//		}
+		
+		System.out.println("================= roadGameData Test =================");
+		dao.roadGameData();
+		
+//		System.out.println(dao.userData);
+		
+		Set key = dao.userData.keySet();
+		Iterator it = key.iterator();
+		
+		while(it.hasNext()) {
+			String id = (String)it.next();
+			Vector<GameDataDto> vector = dao.userData.get(id);
+			
+			System.out.println(id+" data");
+			for(GameDataDto dto : vector) {
+				System.out.println(dto);
+			}
+		}
+	}
+	
+	
+	public static void main_roadData(String[] args) {
 		
 		System.out.println("================== roadUser Test ==================");
 		dao.roadUser();
